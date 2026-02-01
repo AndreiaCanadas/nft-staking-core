6a50aa958b8cb0787353f1df87db35a76b2dd243
Andreia Canadas <36857252+AndreiaCanadas@users.noreply.github.com>
2026-02-01 16:25:17 +0700

    mint nft added

--- programs/nft-staking-core/src/lib.rs
+++ programs/nft-staking-core/src/lib.rs
@@ -15,7 +15,16 @@ pub mod nft_staking_core {
         ctx.accounts.create_collection(name, uri, &ctx.bumps)
     }
 
+    pub fn mint_nft(ctx: Context<Mint>, name: String, uri: String) -> Result<()> {
+        ctx.accounts.mint_nft(name, uri, &ctx.bumps)
+    }
+
     pub fn initialize_config(ctx: Context<Initialize>, points_per_stake: u8, freeze_period: u8) -> Result<()> {
         ctx.accounts.init_config(points_per_stake, freeze_period, &ctx.bumps)
     }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        ctx.accounts.stake()
+    }
+
 }
